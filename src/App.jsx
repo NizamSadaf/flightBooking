915d96ad0bf9db8f364973122cd5178cf389a0b4
NizamSadaf <nizamsadaf1460@gmail.com>
2023-02-23 01:42:17 +0600

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Flights from './components/Flights'
 import Flights_input from './components/Flights_input'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import logo from './assets/lws-logo.svg'
 function App() {
   return (
     <Provider store={store}>
           <header id="header">
         <div class="container">
-          <img src="./src/assets/lws-logo.svg" alt="logo" class="logo" />
+          <img src={logo} alt="logo" class="logo" />
           <div class="flex items-center">
             <a class="text-white min-w-[50px] font-medium" href="#">Home</a>
             <button class="log-btn btn">Login</button>
